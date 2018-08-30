e5f080139b2b0f13dc593daacafc6a4e0f06fd08
Allison Tearjen <amtearjen@ucdavis.edu>
2018-08-30 19:52:12 +0000

    automatically backed up by learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -2,7 +2,7 @@
 # See README.md for instructions on how to do this
 def fizzbuzz
   if int % 3 == 0
-    "Fizz"
+    return "Fizz"
   elsif int % 5 == 0
     "Buzz"
   elsif (int % 5 == 0) && (int % 3 == 0)
